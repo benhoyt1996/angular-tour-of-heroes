8d836b7ab6152eb4c8842afa6a93cc65eb38570a
Ben Hoyt <nonmom64@gmail.com>
2018-11-03 01:44:53 -0500

    tutorial part 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service'
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +12,7 @@ import {HEROES} from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
 
-  heroes = HEROES;
+  heroes : Hero[];
 
   selectedHero: Hero;
 
@@ -19,9 +20,15 @@ export class HeroesComponent implements OnInit {
       this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 
 }
