6153b6a97c9cf6b97b597f8e7db07e566bb18bc4
Ben Hoyt <nonmom64@gmail.com>
2018-11-02 23:42:36 -0500

    tutorial part 2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './hero'
+
+export const HEROES: Hero[] = [
+    {id:1, name:'Windstorm'},
+    {id:2, name:'Mr. Bean'},
+    {id:3, name: 'Superman'},
+    {id: 4, name: 'Batman'},
+    {id: 5, name: 'Random hero'}
+];
